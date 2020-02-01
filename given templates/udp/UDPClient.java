26cb52820331decf2897163edeb27f820470961b
unknown <kamilbajdo@gmail.com>
2020-02-01 17:37:00 +0100

    I just couldnt look at it so I made it little prettier. Please dont make ugly code :(

--- given templates/udp/UDPClient.java
+++ given templates/udp/UDPClient.java
@@ -6,20 +6,63 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.security.interfaces.ECKey;
+import java.security.spec.ECFieldFp;
 
 import common.MessageInfo;
 
 public class UDPClient {
-
 	private DatagramSocket sendSoc;
 
+	public UDPClient() {
+		try {
+            sendSoc = new DatagramSocket();
+        } catch(SocketException soEx) {
+			System.out.println("Exception while creating socket " + soEx.getMessage());
+        } catch(SecurityException secEx) {
+			System.out.println("Exception while creating socket " + secEx.getMessage());
+        } catch(Exception e) {
+			System.out.println("Exception while creating socket " + e.getMessage());
+        }
+	}
+
+	private void testLoop(InetAddress serverAddr, int recvPort, int countTo) {
+		int tries = 0;
+
+		try {
+            sendSoc.connect(serverAddr, recvPort);
+            while(tries <= countTo){
+                String payload = countTo + ";" + tries;
+                this.send(payload, serverAddr, recvPort);
+                tries++;
+            }
+        } catch(Exception e) {
+			System.out.println("Exception while connecting " + e.getMessage());
+        }
+	}
+
+	private void send(String payload, InetAddress destAddr, int destPort) {
+		int payloadSize = payload.length();
+		byte[] pktData = payload.getBytes();
+		DatagramPacket pkt = new DatagramPacket(pktData, payloadSize, destAddr, destPort);
+		
+		try {
+            sendSoc.send(pkt);
+		} catch(Exception e) {
+			System.out.println("Exception while sending " + e.getMessage());
+		}
+	}
+
+    public void runLoop(InetAddress serverAddr, int recvPort, int countTo){
+        testLoop(serverAddr, recvPort, countTo);
+	}
+	
 	public static void main(String[] args) {
 		InetAddress	serverAddr = null;
-		int			recvPort;
-		int 		countTo;
-		String 		message;
+		int	recvPort;
+		int countTo;
+		String message;
 
-		// Get the parameters
 		if (args.length < 3) {
 			System.err.println("Arguments required: server name/IP, recv port, message count");
 			System.exit(-1);
@@ -33,53 +76,6 @@ public class UDPClient {
 		}
 		recvPort = Integer.parseInt(args[1]);
 		countTo = Integer.parseInt(args[2]);
-
-
-		// Construct UDP client class and try to send messages
 		UDPClient myClient = new UDPClient();
-		
 	}
-
-	public UDPClient() {
-		// TO-DO: Initialise the UDP socket for sending data
-		try {
-            sendSoc = new DatagramSocket();
-        } catch(SocketException soEx) {
-        } catch(SecurityException secEx) {
-        } catch(Exception e) {
-        }
-	}
-
-	private void testLoop(InetAddress serverAddr, int recvPort, int countTo) {
-		int tries = 0;
-
-		//Send the messages to the server
-		try {
-            sendSoc.connect(serverAddr, recvPort);
-            while(tries <= countTo){
-                String payload = countTo + ";" + tries;
-                this.send(payload, serverAddr, recvPort);
-                tries++;
-            }
-        } catch(Exception e) {
-        }
-	}
-
-	private void send(String payload, InetAddress destAddr, int destPort) {
-		int payloadSize = payload.length();
-		byte[] pktData = payload.getBytes();
-		//build datagram packet
-		DatagramPacket pkt = new DatagramPacket(pktData, payloadSize, destAddr, destPort);
-		
-		//send it to the server
-		try{
-            sendSoc.send(pkt);
-		} catch(Exception e) {
-		}
-		
-	}
-
-    public void runLoop(InetAddress serverAddr, int recvPort, int countTo){
-        testLoop(serverAddr, recvPort, countTo);
-    }
 }
--- given templates/udp/UDPServer.java
+++ given templates/udp/UDPServer.java
@@ -10,65 +10,51 @@ import java.util.Arrays;
 import common.MessageInfo;
 
 public class UDPServer {
-
 	private DatagramSocket recvSoc;
 	private int totalMessages = -1;
 	private int[] receivedMessages;
 	private boolean close;
 
 	private void run() {
-        //set packet size
 		int pacSize = 0;
 		byte[] pacData;
 		DatagramPacket pac;
-		
-		//setting the timeout value (in ms)
         int timeout = 30000;
 		close = false;
 		
 		while(close == false){
-            		//create a new packet of data
-            		pacData = new byte[pacSize];
-            		//make a datagramPacket for this data
-            		pac = new DatagramPacket(pacData, pacSize);
-           		try{
-              		  	/*enables/disables 'so_timeout' in ms, when recieve() is called the 
-              		 	 recvSoc will only block for this amount of time, if the timeout expires 
-              		 	 an exception is raised*/
-              		  	recvSoc.setSoTimeout(timeout);
-              		  	recvSoc.receive(pac);
-               			 //getData returns data buffer, doing new string with this constructor removes any excess and passes only the string required
-                		String dataIn = new String(pac.getData(), pac.getOffset(), pac.getLength());
-                		processMessage(dataIn);
-           		}catch(Exception e) {
-           		}
+			pacData = new byte[pacSize];
+			pac = new DatagramPacket(pacData, pacSize);
+			try {
+				recvSoc.setSoTimeout(timeout);
+				recvSoc.receive(pac);
+				String dataIn = new String(pac.getData(), pac.getOffset(), pac.getLength());
+				processMessage(dataIn);
+			} catch(Exception e) {
+				System.out.println("Exception while receiving data " + e.getMessage());
+			}
 		}
 	}
 
 	public void processMessage(String data) {
-
 		MessageInfo msg = null;
 
-        try{
+        try {
             msg = new MessageInfo(data);
-        }
-        catch(Exception e){
+        } catch(Exception e) {
+			System.out.println("Exception while making new message " + e.getMessage());
         }
         
-		// TO-DO: On receipt of first message, initialise the receive buffer
-		if(totalMessages < 0){
-            //from common/messageInfo
+		if(totalMessages < 0) {
             totalMessages = msg.totalMessages;
             receivedMessages = new int[totalMessages];
             System.out.print("Recieving " + totalMessages + " messages\n");
         }
         
-        //set the message index in the recieved messages array to 1 to indicate they have been recieved
         receivedMessages[msg.messageNum] = 1;
         System.out.print(msg.toString());
 
-		//identifies any missing messages by seeing if their index = 0 and prints the index of missing ones
-		if(totalMessages - msg.messageNum == 1){
+		if(totalMessages - msg.messageNum == 1) {
             close = true;
             System.out.print("\nThe messages that were lost are: ");
             for(int i = 0; i < receivedMessages.length; i++){
@@ -77,36 +63,26 @@ public class UDPServer {
                 }
             }
 
-	}
+		}
 	}
 
-
-	public UDPServer(int rp) {
-		
-		//constructs a datagram socket and binds it to the specified port on the local host machince
-        try{
-            recvSoc = new DatagramSocket(rp);
+	public UDPServer(int port) {
+        try {
+            recvSoc = new DatagramSocket(port);
         } catch(Exception e) {
+			System.out.println("Exception while creating new socket " + e.getMessage());
         }
-        
-        
-		// Done Initialisation
-		System.out.println("UDPServer ready");
 	}
 
 	public static void main(String args[]) {
-		int	recvPort;
-
-		// Get the parameters from command line
 		if (args.length < 1) {
 			System.err.println("Arguments required: recv port");
 			System.exit(-1);
 		}
-		recvPort = Integer.parseInt(args[0]);
+		int recvPort = Integer.parseInt(args[0]);
 
 		// TO-DO: Construct Server object and start it by calling run().
 		UDPServer myServer = new UDPServer(recvPort);
 		myServer.run();
 	}
-
 }
