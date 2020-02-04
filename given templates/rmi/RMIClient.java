a4efbad86798696d944c16db1048cc51d32c50fc
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-02-04 16:54:49 +0000

    Update RMIClient.java

--- given templates/rmi/RMIClient.java
+++ given templates/rmi/RMIClient.java
@@ -3,6 +3,10 @@ package rmi;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.net.MalformedURLException;
+
 
 import common.MessageInfo;
 
@@ -14,24 +18,29 @@ public class RMIClient {
 			System.exit(-1);
 		}
 
-		String urlServer = new String("rmi://" + args[0] + "/RMIServer");
+		//String urlServer = new String("rmi://" + args[0] + "/RMIServer");
 		int numMessages = Integer.parseInt(args[1]);
 
 		// TO-DO: Initialise Security Manager
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
 		}
-		
+
 		// TO-DO: Bind to RMIServer
 		try {
-			RMIServerI bigboi = (RMIServerI)Naming.lookup(urlServer);
-            for(int i = 0; i < numMessages; i++){
-			bigboi.receiveMessage(new MessageInfo(numMessages, i));
+			Registry r = LocateRegistry.getRegistry(args[0]);
+			iRMIServer = (RMIServerI)Naming.lookup("fred");
+
+      for(int i = 0; i < numMessages; i++){
+				iRMIServer.receiveMessage(new MessageInfo(numMessages, i));
+		  }
 		}
-		} catch (Exception e) {
+		catch (RemoteException | NotBoundException | MalformedURLException e) {
 			System.out.println("Exception while receiving message " + e.getMessage());
+			System.exit(-1);
 		}
-		
+
 		// TO-DO: Attempt to send messages the specified number of times
 	}
 }
+
